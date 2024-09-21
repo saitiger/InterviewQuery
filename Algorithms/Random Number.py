0b2a62a046c29a0d9bf9331235f8aa85b46fe6d7
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-09-20 19:24:41 -0700

    Update Random Number.py

--- Algorithms/Random Number.py
+++ Algorithms/Random Number.py
@@ -1,9 +1,12 @@
 import random
-
-def random_number(x, y=None, count=1):
-    count += 1
-
+def random_number(x, y=0, count=1):
+    # For the first element, select it with probability 1
+    if count == 1:
+        return x
+    # For subsequent elements, select the new element with probability 1/count
     if random.randint(1, count) == 1:
-        y = x  
-      
-    return y, count
+        return x
+    return y
+
+# Reservoir Sampling : 
+https://www.geeksforgeeks.org/reservoir-sampling/
